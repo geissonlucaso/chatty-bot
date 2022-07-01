1c3c74793d136fd76ee65204418cfd2f28534e49
Geisson de Oliveira <geissonlucaso@gmail.com>
2022-07-01 15:42:12 -0300

    Setting the username input.

--- ChattyBot/src/main/kotlin/ChattyBot.kt
+++ ChattyBot/src/main/kotlin/ChattyBot.kt
@@ -1,4 +1,17 @@
+import java.util.*      //  Java library.
+
 fun main(){
+
+    // Variables.
+    val scanner = Scanner(System.`in`)
+
     println("Hello, my name is Aid.")
     println("I was created in 2022.")
+    println("Please, remind me your name.")
+
+    // reading a name
+    var name = scanner.nextLine()
+
+    println("What a great name you have, $name!")
+    
 }
